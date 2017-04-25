0746c8de066d6973c287e5aeffe078228fdf9a61
Onyemobi <onyemobi.anyiwo@gmail.com>
2017-04-25 19:40:09 -0400

    merge stream added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require ('gulp');
 var sass = require ('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var browserify = require('gulp-browserify');
 var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 var SOURCEPATHS = {
     sassSource : 'src/scss/*.scss',
@@ -29,15 +31,23 @@ gulp.task('clean-scripts', function(){
 } );
 
 gulp.task ('sass', function(){
-    return gulp.src(SOURCEPATHS.sassSource)
-    .pipe(autoprefixer())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) 
-    .pipe(gulp.dest(APPPATH.css));
+
+    var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+    var sassFiles; 
+
+    sassFiles =  gulp.src(SOURCEPATHS.sassSource)
+        .pipe(autoprefixer())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) 
+    
+    return merge(sassFiles, bootstrapCSS)
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest(APPPATH.css));
 });
 
 gulp.task('scripts', ['clean-scripts'], function(){
     gulp.src(SOURCEPATHS.jsSource)
         .pipe(concat('main.js'))
+        .pipe(browserify())
         .pipe(gulp.dest(APPPATH.js))
 });
 
